2d67648a3c03fd1015d15efe86f4a0429371a87f
gdever <alexey@dodonov.pro>
2021-02-04 08:49:44 +0300

    feature/Refactoring

---
+++ Mezon/Session/Layer.php
@@ -0,0 +1,41 @@
+<?php
+namespace Mezon\Session;
+
+use Mezon\Conf\Conf;
+
+class Layer
+{
+
+    /**
+     * Was the session started
+     *
+     * @var boolean
+     */
+    private static $sessionWasStarted = false;
+
+    /**
+     * Method starts session
+     *
+     * @return bool true if the session was started, false otherwise
+     */
+    public static function startSession(): bool
+    {
+        if (Conf::getConfigValue('redirect/layer', 'real') === 'real') {
+            if (! self::$sessionWasStarted) {
+                return self::$sessionWasStarted = session_start();
+            }
+        } else {
+            self::$sessionWasStarted = true;
+        }
+    }
+
+    /**
+     * Method returns information was the session started
+     *
+     * @return bool true if the session was started, false otherwise.
+     */
+    public static function wasSessionStarted(): bool
+    {
+        return self::$sessionWasStarted;
+    }
+}
