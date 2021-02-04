5b0a582c83856a6b9c18e077cef37b0163ec188b
gdever <alexey@dodonov.pro>
2021-02-04 08:43:57 +0300

    feature/Session start routine was added

--- Mezon/Redirect/Layer.php
+++ Mezon/Redirect/Layer.php
@@ -25,4 +25,35 @@ class Layer
             self::$redirectWasPerformed = true;
         }
     }
+
+    /**
+     * Was the session started
+     *
+     * @var boolean
+     */
+    private static $sessionWasStarted = false;
+
+    /**
+     * Method starts session
+     *
+     * @return bool true if the session was started, false otherwise
+     */
+    public static function startSession(): bool
+    {
+        if (Conf::getConfigValue('redirect/layer', 'real') === 'real') {
+            return self::$sessionWasStarted = session_start();
+        } else {
+            self::$sessionWasStarted = true;
+        }
+    }
+
+    /**
+     * Method returns information was the session started
+     *
+     * @return bool true if the session was started, false otherwise.
+     */
+    public static function wasSessionStarted(): bool
+    {
+        return self::$sessionWasStarted;
+    }
 }
